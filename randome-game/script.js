6dc1fd3d4f155160d411156df47cd5f273f9e5df
Anna Petrova <ann.undine@gmail.com>
2024-10-09 14:42:51 +0200

    feat: add footer and block for results

--- randome-game/script.js
+++ randome-game/script.js
@@ -10,17 +10,25 @@ const foreground = new Image();
 
 const gap = 140;
 let score = 0;
-const gravity = 1.3;
+let gravity = 1.3;
+let isGameOver = false;
+let flyTimeOut = null;
 
 const button = {
     x: 200,
     y: 180,
     width: 150,
-    height: 50,
-  };
+    height: 50
+};
 
-birdUp.src = 'assets/bird-up.png';
-birdDown.src = 'assets/bird-up.png';
+const results = {
+    x: 650,
+    y: 0,
+    width: 250,
+    height: canvas.height
+}
+
+birdDown.src = 'assets/bird-down.png';
 background.src = 'assets/background.jpg';
 pipeYellow.src = 'assets/pipe-yellow.png';
 foreground.src = 'assets/floor-sprite.png';
@@ -32,7 +40,13 @@ let yPosition = 100;
 document.addEventListener('keydown', moveUp);
 
 function moveUp() {
-    yPosition -= 25;
+    gravity = -1.3;
+    birdDown.src = 'assets/bird-up.png';
+    clearTimeout(flyTimeOut);
+    flyTimeOut = setTimeout(()=>{
+        birdDown.src = 'assets/bird-down.png';
+        gravity = 1.3;
+    }, 500);
 }
 
 const pipe = [];
@@ -41,14 +55,14 @@ pipe[0] = {
     y: 0
 }
 
-function draw () {
-    context.drawImage(background, 0, 0); 
+function draw() {
+    context.drawImage(background, 0, 0);
 
-    for(let i = 0; i < pipe.length; i++) {
+    for (let i = 0; i < pipe.length; i++) {
         context.drawImage(pipeYellow, pipe[i].x, pipe[i].y);
         context.drawImage(pipeYellow, pipe[i].x, pipe[i].y + pipeYellow.height + gap);
 
-        pipe[i].x--; 
+        pipe[i].x--;
 
         if (pipe[i].x == 500) {
             pipe.push({
@@ -56,39 +70,64 @@ function draw () {
                 y: Math.floor(Math.random() * pipeYellow.height) - pipeYellow.height
             })
         }
-        /// track touches
-        if (xPosition + birdDown.width >= pipe[i].x 
+        /// track loses
+        if (xPosition + birdDown.width >= pipe[i].x
             && xPosition <= pipe[i].x + pipeYellow.width
-            && (yPosition <= pipe[i].y + pipeYellow.height 
-                || yPosition + birdDown.height >= pipe[i].y + pipeYellow.height + gap) 
-                || yPosition + birdDown.height >= canvas.height - foreground.height
-            ) {
-                context.fillStyle = "#900C3F";  
-                context.font = "38px Arial";
-                context.fillText("Game Over!", 200, 100);
-                context.fillText("Your score is: " + score, 200, 150);
+            && (yPosition <= pipe[i].y + pipeYellow.height
+                || yPosition + birdDown.height >= pipe[i].y + pipeYellow.height + gap)
+            || yPosition + birdDown.height >= canvas.height - foreground.height
+        ) {
+            isGameOver = true;
+            context.fillStyle = "#900C3F";
+            context.font = "38px Arial";
+            context.fillText("Game Over!", 200, 100);
+            context.fillText("Your score is: " + score, 200, 150);
 
-                context.fillRect(button.x, button.y, button.width, button.height);
-                context.fillStyle = 'white';
-                context.font = '24px Arial';
-                context.fillText('Restart', button.x + 15, button.y + button.height / 2 + 8);
+            context.fillRect(button.x, button.y, button.width, button.height);
+            context.fillStyle = 'white';
+            context.font = '24px Arial';
+            context.fillText('Restart', button.x + 15, button.y + button.height / 2 + 8);
 
-                return;
-            }
-        
-        if(pipe[i].x === 120) {
+            return;
+        }
+
+        if (pipe[i].x === 120) {
             score += 10;
+        }
+
+        /// track victory
+        if(score === 50) {
+            isGameOver = true;
+            context.fillStyle = "#900C3F";
+            context.font = "38px Arial";
+            context.fillText("Congratulations! You win!", 200, 100);
+            context.fillText("Your score is: " + score, 200, 150);
+
+            context.fillRect(button.x, button.y, button.width, button.height);
+            context.fillStyle = 'white';
+            context.font = '24px Arial';
+            context.fillText('Restart', button.x + 15, button.y + button.height / 2 + 8);
+            return;
         }
     }
 
+    /// Results
+
+    context.fillStyle = '#dcda96';
+    context.fillRect(results.x, results.y, results.width, results.height);
+    context.fillStyle = '#4040FF';
+    context.font = '24px Arial';
+    context.fillText('Resulst', results.x + 15, results.y + 60);
+
+    /// Draw foreground
     context.drawImage(foreground, 0, canvas.height - foreground.height);
     context.drawImage(birdDown, xPosition, yPosition);
-
-    yPosition += gravity;
-
     context.fillStyle = '#4040FF';
     context.font = '23px Roboto';
-    context.fillText('Score: ' + score, 10, canvas.height - 30);
+    context.fillText('Your goal: 50', 10, canvas.height - 40);
+    context.fillText('Your score: ' + score, 10, canvas.height - 15);
+
+    yPosition += gravity;
 
     requestAnimationFrame(draw);
 }
