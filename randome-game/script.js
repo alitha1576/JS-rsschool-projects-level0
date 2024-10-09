c49936e0861d98d4b5e96c00494ebf98dcb3e61c
Anna Petrova <ann.undine@gmail.com>
2024-10-09 12:30:04 +0200

    feat: add game over window

--- randome-game/script.js
+++ randome-game/script.js
@@ -1,14 +1,23 @@
 const canvas = document.getElementById('canvas');
 const context = canvas.getContext('2d');
+
 const birdUp = new Image();
 const birdDown = new Image();
 const background = new Image();
 const footer = new Image();
 const pipeYellow = new Image();
 const foreground = new Image();
+
 const gap = 140;
 let score = 0;
-const gravity = 1.4;
+const gravity = 1.3;
+
+const button = {
+    x: 200,
+    y: 180,
+    width: 150,
+    height: 50,
+  };
 
 birdUp.src = 'assets/bird-up.png';
 birdDown.src = 'assets/bird-up.png';
@@ -23,12 +32,12 @@ let yPosition = 100;
 document.addEventListener('keydown', moveUp);
 
 function moveUp() {
-    yPosition -= 20;
+    yPosition -= 25;
 }
 
 const pipe = [];
 pipe[0] = {
-    x: canvas.width,
+    x: canvas.width - 180,
     y: 0
 }
 
@@ -39,7 +48,7 @@ function draw () {
         context.drawImage(pipeYellow, pipe[i].x, pipe[i].y);
         context.drawImage(pipeYellow, pipe[i].x, pipe[i].y + pipeYellow.height + gap);
 
-        pipe[i].x--;
+        pipe[i].x--; 
 
         if (pipe[i].x == 500) {
             pipe.push({
@@ -54,7 +63,17 @@ function draw () {
                 || yPosition + birdDown.height >= pipe[i].y + pipeYellow.height + gap) 
                 || yPosition + birdDown.height >= canvas.height - foreground.height
             ) {
-                location.reload();
+                context.fillStyle = "#900C3F";  
+                context.font = "38px Arial";
+                context.fillText("Game Over!", 200, 100);
+                context.fillText("Your score is: " + score, 200, 150);
+
+                context.fillRect(button.x, button.y, button.width, button.height);
+                context.fillStyle = 'white';
+                context.font = '24px Arial';
+                context.fillText('Restart', button.x + 15, button.y + button.height / 2 + 8);
+
+                return;
             }
         
         if(pipe[i].x === 120) {
