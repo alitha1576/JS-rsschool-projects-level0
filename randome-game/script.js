ffa872a7f0ad0dea19290b7dd647f8e94f98e2b8
Anna Petrova <ann.undine@gmail.com>
2024-10-09 11:41:45 +0200

    feat: add score count

--- randome-game/script.js
+++ randome-game/script.js
@@ -5,21 +5,20 @@ const birdDown = new Image();
 const background = new Image();
 const footer = new Image();
 const pipeYellow = new Image();
-const pipeGreen = new Image();
 const foreground = new Image();
-const gap = 100;
+const gap = 140;
+let score = 0;
+const gravity = 1.4;
 
 birdUp.src = 'assets/bird-up.png';
 birdDown.src = 'assets/bird-up.png';
 background.src = 'assets/background.jpg';
 pipeYellow.src = 'assets/pipe-yellow.png';
-pipeGreen.src = 'assets/pipe-green.png';
 foreground.src = 'assets/floor-sprite.png';
 
 //// bird position
-const xPosition = 15;
+const xPosition = canvas.width * 0.3;
 let yPosition = 100;
-const gravity = 1;
 
 document.addEventListener('keydown', moveUp);
 
@@ -38,7 +37,7 @@ function draw () {
 
     for(let i = 0; i < pipe.length; i++) {
         context.drawImage(pipeYellow, pipe[i].x, pipe[i].y);
-        context.drawImage(pipeGreen, pipe[i].x, pipe[i].y + pipeYellow.height + gap);
+        context.drawImage(pipeYellow, pipe[i].x, pipe[i].y + pipeYellow.height + gap);
 
         pipe[i].x--;
 
@@ -48,19 +47,30 @@ function draw () {
                 y: Math.floor(Math.random() * pipeYellow.height) - pipeYellow.height
             })
         }
-
+        /// track touches
         if (xPosition + birdDown.width >= pipe[i].x 
             && xPosition <= pipe[i].x + pipeYellow.width
             && (yPosition <= pipe[i].y + pipeYellow.height 
-                || yPosition + birdDown.height >= pipe[i].y + pipeYellow.height + gap)
+                || yPosition + birdDown.height >= pipe[i].y + pipeYellow.height + gap) 
+                || yPosition + birdDown.height >= canvas.height - foreground.height
             ) {
                 location.reload();
             }
+        
+        if(pipe[i].x === 120) {
+            score += 10;
+        }
     }
 
     context.drawImage(foreground, 0, canvas.height - foreground.height);
     context.drawImage(birdDown, xPosition, yPosition);
+
     yPosition += gravity;
+
+    context.fillStyle = '#4040FF';
+    context.font = '23px Roboto';
+    context.fillText('Score: ' + score, 10, canvas.height - 30);
+
     requestAnimationFrame(draw);
 }
 
