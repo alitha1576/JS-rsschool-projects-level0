3f7c43d6977da4b23a337104027b23f803146d1d
Anna Petrova <ann.undine@gmail.com>
2024-10-09 10:51:02 +0200

    feat: add images and basic logic to the  game

---
+++ randome-game/script.js
@@ -0,0 +1,67 @@
+const canvas = document.getElementById('canvas');
+const context = canvas.getContext('2d');
+const birdUp = new Image();
+const birdDown = new Image();
+const background = new Image();
+const footer = new Image();
+const pipeYellow = new Image();
+const pipeGreen = new Image();
+const foreground = new Image();
+const gap = 100;
+
+birdUp.src = 'assets/bird-up.png';
+birdDown.src = 'assets/bird-up.png';
+background.src = 'assets/background.jpg';
+pipeYellow.src = 'assets/pipe-yellow.png';
+pipeGreen.src = 'assets/pipe-green.png';
+foreground.src = 'assets/floor-sprite.png';
+
+//// bird position
+const xPosition = 15;
+let yPosition = 100;
+const gravity = 1;
+
+document.addEventListener('keydown', moveUp);
+
+function moveUp() {
+    yPosition -= 20;
+}
+
+const pipe = [];
+pipe[0] = {
+    x: canvas.width,
+    y: 0
+}
+
+function draw () {
+    context.drawImage(background, 0, 0); 
+
+    for(let i = 0; i < pipe.length; i++) {
+        context.drawImage(pipeYellow, pipe[i].x, pipe[i].y);
+        context.drawImage(pipeGreen, pipe[i].x, pipe[i].y + pipeYellow.height + gap);
+
+        pipe[i].x--;
+
+        if (pipe[i].x == 500) {
+            pipe.push({
+                x: canvas.width,
+                y: Math.floor(Math.random() * pipeYellow.height) - pipeYellow.height
+            })
+        }
+
+        if (xPosition + birdDown.width >= pipe[i].x 
+            && xPosition <= pipe[i].x + pipeYellow.width
+            && (yPosition <= pipe[i].y + pipeYellow.height 
+                || yPosition + birdDown.height >= pipe[i].y + pipeYellow.height + gap)
+            ) {
+                location.reload();
+            }
+    }
+
+    context.drawImage(foreground, 0, canvas.height - foreground.height);
+    context.drawImage(birdDown, xPosition, yPosition);
+    yPosition += gravity;
+    requestAnimationFrame(draw);
+}
+
+foreground.onload = draw;
